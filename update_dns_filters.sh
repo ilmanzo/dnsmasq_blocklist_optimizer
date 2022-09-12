9493cc6bf4edca7413d41de5639a5b4757782c4e
Andrea Manzini <ilmanzo@gmail.com>
2022-09-12 16:35:20 +0200

    Update update_dns_filters.sh

--- update_dns_filters.sh
+++ update_dns_filters.sh
@@ -2,16 +2,20 @@
 #update file hosts with blacklist
 
 set -u 
-set -e
+#set -e
 
 TMPDIR="$(mktemp -d)"
 trap 'rm -rf -- "$TMPDIR"' EXIT
 
 pushd $TMPDIR
+wget -q https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts &
+wget -q https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt &
+wget -q https://dbl.oisd.nl/ &
 # download list of lists to download
 curl https://v.firebog.net/hosts/lists.php?type=tick -o urls.txt
 # gets actual files
-xargs -P 2 -n 1 curl -s -O < urls.txt
+for u in $(cat urls.txt); do wget -q $u ; done
+wait
 rm urls.txt
 /usr/local/bin/gen_dns_blocklist.py * > /etc/blocked.hosts
 systemctl restart dnsmasq
